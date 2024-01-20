892234307d1dd9d7506efafae90c8b1c00b77963
enn3 <arturpeshko39@gmail.com>
2024-01-20 20:36:32 +0100

    better debug

--- src/main.rs
+++ src/main.rs
@@ -104,7 +104,6 @@ fn replace_all_old_file(
             if let ChunkMessage::CHUNK(chunk_data) = message {
                 let chunk: Result<chunk147::Chunk> = fastnbt::from_bytes(&chunk_data);
                 if let Ok(mut chunk) = chunk {
-                    println!("Converting chunk");
                     for section in chunk.mut_level().mut_sections().iter_mut() {
                         for (key, value) in conversion_map.read().unwrap().iter() {
                             if let Some(data) = data_map.read().unwrap().get(key) {
