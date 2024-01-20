223a33e63589462131a62053c6ce26cd7fff6674
enn3 <arturpeshko39@gmail.com>
2024-01-20 18:51:43 +0100

    better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use convert_world::{chunk1201, chunk147};
-use fastanvil::Region;
+use convert_world::chunk147;
+use fastanvil::{Error, Region};
 use fastnbt::error::Result;
 use std::collections::HashMap;
 use std::fs::{File, OpenOptions};
@@ -8,60 +8,60 @@ use std::path::PathBuf;
 use std::sync::{Arc, RwLock};
 use std::{fs, thread};
 
-fn convert_to_new() {
-    let path = std::env::args().nth(1).unwrap();
-    let conversion_path = std::env::args().nth(2).unwrap();
-    let converted_path = std::env::args().nth(3).unwrap();
-    let file = File::open(path).unwrap();
-    println!("Opening {}", conversion_path);
-    let mut conversion_file = File::open(conversion_path).unwrap();
-    let mut open_options = OpenOptions::new();
-    open_options
-        .write(true)
-        .create(true)
-        .truncate(true)
-        .read(true);
-    let converted_file = open_options.open(converted_path).unwrap();
-
-    let mut mca = Region::from_stream(file).unwrap();
-    let mut converted_mca = Region::new(converted_file).unwrap();
-    let mut conversion_map = HashMap::new();
-
-    let mut conversion_content = String::new();
-    println!("Reading conversion file");
-    conversion_file
-        .read_to_string(&mut conversion_content)
-        .unwrap();
-
-    for line in conversion_content.lines() {
-        let mut split = line.split("->");
-        let old_id = split
-            .nth(0)
-            .unwrap()
-            .trim()
-            .parse::<chunk147::Block>()
-            .unwrap();
-        let new_id = split.nth(0).unwrap().trim().to_string();
-        conversion_map.insert(old_id, new_id);
-    }
-
-    for (idx, chunk) in mca.iter().flatten().enumerate() {
-        println!("Converted {} chunks", idx);
-        let chunk: Result<chunk147::Chunk> = fastnbt::from_bytes(&chunk.data);
-        let chunk = chunk.unwrap();
-        let new_chunk = chunk1201::Chunk::convert_old(&chunk, &conversion_map);
-        converted_mca
-            .write_chunk(
-                chunk.level().x_pos() as usize,
-                chunk.level().z_pos() as usize,
-                fastnbt::to_bytes(&new_chunk)
-                    .expect("can't convert chunk to bytes")
-                    .as_slice(),
-            )
-            .expect("can't write chunk to file");
-    }
-    println!("Converted all chunks");
-}
+// fn convert_to_new() {
+//     let path = std::env::args().nth(1).unwrap();
+//     let conversion_path = std::env::args().nth(2).unwrap();
+//     let converted_path = std::env::args().nth(3).unwrap();
+//     let file = File::open(path).unwrap();
+//     println!("Opening {}", conversion_path);
+//     let mut conversion_file = File::open(conversion_path).unwrap();
+//     let mut open_options = OpenOptions::new();
+//     open_options
+//         .write(true)
+//         .create(true)
+//         .truncate(true)
+//         .read(true);
+//     let converted_file = open_options.open(converted_path).unwrap();
+//
+//     let mut mca = Region::from_stream(file).unwrap();
+//     let mut converted_mca = Region::new(converted_file).unwrap();
+//     let mut conversion_map = HashMap::new();
+//
+//     let mut conversion_content = String::new();
+//     println!("Reading conversion file");
+//     conversion_file
+//         .read_to_string(&mut conversion_content)
+//         .unwrap();
+//
+//     for line in conversion_content.lines() {
+//         let mut split = line.split("->");
+//         let old_id = split
+//             .nth(0)
+//             .unwrap()
+//             .trim()
+//             .parse::<chunk147::Block>()
+//             .unwrap();
+//         let new_id = split.nth(0).unwrap().trim().to_string();
+//         conversion_map.insert(old_id, new_id);
+//     }
+//
+//     for (idx, chunk) in mca.iter().flatten().enumerate() {
+//         println!("Converted {} chunks", idx);
+//         let chunk: Result<chunk147::Chunk> = fastnbt::from_bytes(&chunk.data);
+//         let chunk = chunk.unwrap();
+//         let new_chunk = chunk1201::Chunk::convert_old(&chunk, &conversion_map);
+//         converted_mca
+//             .write_chunk(
+//                 chunk.level().x_pos() as usize,
+//                 chunk.level().z_pos() as usize,
+//                 fastnbt::to_bytes(&new_chunk)
+//                     .expect("can't convert chunk to bytes")
+//                     .as_slice(),
+//             )
+//             .expect("can't write chunk to file");
+//     }
+//     println!("Converted all chunks");
+// }
 
 enum ChunkMessage {
     JOIN,
@@ -158,15 +158,15 @@ fn replace_all_old_file(path: String, conversion_path: String, converted_path: S
 
     while let Ok(chunk) = rx_r.recv() {
         converted += 1;
-        converted_mca
-            .write_chunk(
-                chunk.level().x_pos() as usize,
-                chunk.level().z_pos() as usize,
-                fastnbt::to_bytes(&chunk)
-                    .expect("can't convert chunk to bytes")
-                    .as_slice(),
-            )
-            .expect("can't write chunk to file");
+        if let Err(Error::InvalidOffset(x, z)) = converted_mca.write_chunk(
+            chunk.level().x_pos() as usize,
+            chunk.level().z_pos() as usize,
+            fastnbt::to_bytes(&chunk)
+                .expect("can't convert chunk to bytes")
+                .as_slice(),
+        ) {
+            println!("can't write chunk at x: {x}, z: {z}");
+        }
         if converted == read {
             break;
         }
