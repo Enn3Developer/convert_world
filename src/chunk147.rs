e8f92ae8ab68126fb1b0ed7ee82f65f48310babf
enn3 <arturpeshko39@gmail.com>
2024-01-21 20:12:33 +0100

    better conversion

--- src/chunk147.rs
+++ src/chunk147.rs
@@ -1,20 +1,35 @@
 use fastnbt::{ByteArray, IntArray, Value};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::sync::{Arc, RwLock};
 
-pub type Block = i32;
-
-pub fn convert_block_to_i8(block: &Block) -> i8 {
-    (block & 255) as i8
+#[derive(Eq, Hash, PartialEq)]
+pub struct Block {
+    id: i32,
+    data: Option<i8>,
 }
 
-pub fn convert_i8_to_block(block: i8) -> Block {
-    let mut b = 0 as Block;
+impl Block {
+    pub fn new(id: i32, data: Option<i8>) -> Self {
+        Self { id, data }
+    }
 
-    b |= (block & 0b01111111) as Block;
-    b |= (((block >> 7) as Block) & 0b1) << 7;
+    pub fn to_i8(&self) -> i8 {
+        (self.id & 255) as i8
+    }
 
-    b
+    pub fn add_to_i8(&self) -> i8 {
+        ((self.id & 0b111100000000) >> 8) as i8
+    }
+
+    pub fn from_i8(block: i8) -> Self {
+        let mut id = 0 as i32;
+
+        id |= (block & 0b01111111) as i32;
+        id |= (((block >> 7) as i32) & 0b1) << 7;
+
+        Self { id, data: None }
+    }
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
@@ -194,93 +209,75 @@ impl Section {
 
     pub fn block(&self, x: i32, y: i32, z: i32) -> Block {
         let block_pos = (y * 16 * 16 + z * 16 + x) as usize;
-        let mut block = self.blocks[block_pos] as Block;
+        let mut block = self.blocks[block_pos] as i32;
         if let Some(added) = &self.add {
             block += ((added[block_pos / 2] as i32) >> (4 * (block_pos % 2))) << 8;
         }
-        block
-    }
-
-    fn replace_all_simple(&mut self, old: i8, new: i8) {
-        for block in self.blocks.iter_mut() {
-            if *block == old {
-                *block = new;
-            }
+        let mut data = None;
+        if let Some(data_arr) = &self.data {
+            let d = if block_pos % 2 == 0 {
+                data_arr[block_pos / 2] & 0x0F
+            } else {
+                (data_arr[block_pos / 2] >> 4) & 0x0F
+            };
+            data = Some(d);
         }
+
+        Block { id: block, data }
     }
 
-    pub fn replace_all(&mut self, old: Block, new: Block) {
-        if old < 256 {
-            self.replace_all_simple(convert_block_to_i8(&old), convert_block_to_i8(&new));
-        } else {
-            if let Some(added) = &mut self.add {
-                for (idx, block) in self.blocks.iter_mut().enumerate() {
-                    let b = convert_i8_to_block(*block);
+    pub fn replace_all_blocks(&mut self, conversion_map: Arc<RwLock<HashMap<Block, Block>>>) {
+        if let Some(added) = &self.add {
+            for (idx, block) in self.blocks.iter_mut().enumerate() {
+                let mut b = Block::from_i8(*block);
+                let d = if idx % 2 == 0 {
+                    added[idx / 2] & 0x0F
+                } else {
+                    (added[idx / 2] >> 4) & 0x0F
+                };
+                b.id += (d as i32) << 8;
+                if let Some(data_arr) = &self.data {
                     let d = if idx % 2 == 0 {
-                        added[idx / 2] & 0x0F
+                        data_arr[idx / 2] & 0x0F
                     } else {
-                        (added[idx / 2] >> 4) & 0x0F
+                        (data_arr[idx / 2] >> 4) & 0x0F
                     };
-                    let id = b + ((d as Block) << 8);
-                    if id == old {
-                        *block = convert_block_to_i8(&new);
-                        added[idx / 2] -= d << (4 * (idx % 2));
+                    b.data = Some(d);
+                }
+                if let Some(new_block) = conversion_map.read().unwrap().get(&b) {
+                    self.replace_block(idx, new_block);
+                } else {
+                    b.data = None;
+                    if let Some(new_block) = conversion_map.read().unwrap().get(&b) {
+                        self.replace_block(idx, new_block);
                     }
                 }
             }
         }
     }
 
-    pub fn replace_all_data_simple(&mut self, old: i8, new: i8, new_data: i8) {
-        for (idx, block) in self.blocks.iter_mut().enumerate() {
-            if *block == old {
-                *block = new;
-                if let Some(data) = &mut self.data {
-                    let (mask, shift) = if idx % 2 == 0 {
-                        (0b1111, 0)
-                    } else {
-                        (-0b1110000, 4)
-                    };
-                    data[idx / 2] &= !mask;
-                    data[idx / 2] |= new_data << shift;
-                }
+    fn replace_block(&mut self, idx: usize, new_block: &Block) {
+        if let Some(new_data) = new_block.data {
+            if let Some(data_arr) = &self.data {
+                let (mask, shift) = if idx % 2 == 0 {
+                    (0b1111, 0)
+                } else {
+                    (-0b1110000, 4)
+                };
+                data_arr[idx / 2] &= !mask;
+                data_arr[idx / 2] |= new_data << shift;
             }
         }
-    }
 
-    pub fn replace_all_data(&mut self, old: Block, new: Block, new_data: i8) {
-        if old < 256 {
-            self.replace_all_data_simple(
-                convert_block_to_i8(&old),
-                convert_block_to_i8(&new),
-                new_data,
-            );
-        } else {
-            if let Some(added) = &mut self.add {
-                for (idx, block) in self.blocks.iter_mut().enumerate() {
-                    let b = convert_i8_to_block(*block);
-                    let d = if idx % 2 == 0 {
-                        added[idx / 2] & 0x0F
-                    } else {
-                        (added[idx / 2] >> 4) & 0x0F
-                    };
-                    let id = b + ((d as Block) << 8);
-                    if id == old {
-                        *block = convert_block_to_i8(&new);
-                        added[idx / 2] -= d << (4 * (idx % 2));
-                        if let Some(data) = &mut self.data {
-                            let (mask, shift) = if idx % 2 == 0 {
-                                (0b1111, 0)
-                            } else {
-                                (-0b1110000, 4)
-                            };
-                            data[idx / 2] &= !mask;
-                            data[idx / 2] |= new_data << shift;
-                            // data[idx / 2].set_bits(range, new_data);
-                        }
-                    }
-                }
-            }
+        if let Some(added) = &self.add {
+            let (mask, shift) = if idx % 2 == 0 {
+                (0b1111, 0)
+            } else {
+                (-0b1110000, 4)
+            };
+            self.blocks[idx] = new_block.to_i8();
+            added[idx / 2] &= !mask;
+            added[idx / 2] |= new_block.add_to_i8() << shift;
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,61 +10,6 @@ use std::sync::{Arc, RwLock};
 use std::time::Duration;
 use std::{fs, thread};
 
-// fn convert_to_new() {
-//     let path = std::env::args().nth(1).unwrap();
-//     let conversion_path = std::env::args().nth(2).unwrap();
-//     let converted_path = std::env::args().nth(3).unwrap();
-//     let file = File::open(path).unwrap();
-//     println!("Opening {}", conversion_path);
-//     let mut conversion_file = File::open(conversion_path).unwrap();
-//     let mut open_options = OpenOptions::new();
-//     open_options
-//         .write(true)
-//         .create(true)
-//         .truncate(true)
-//         .read(true);
-//     let converted_file = open_options.open(converted_path).unwrap();
-//
-//     let mut mca = Region::from_stream(file).unwrap();
-//     let mut converted_mca = Region::new(converted_file).unwrap();
-//     let mut conversion_map = HashMap::new();
-//
-//     let mut conversion_content = String::new();
-//     println!("Reading conversion file");
-//     conversion_file
-//         .read_to_string(&mut conversion_content)
-//         .unwrap();
-//
-//     for line in conversion_content.lines() {
-//         let mut split = line.split("->");
-//         let old_id = split
-//             .nth(0)
-//             .unwrap()
-//             .trim()
-//             .parse::<chunk147::Block>()
-//             .unwrap();
-//         let new_id = split.nth(0).unwrap().trim().to_string();
-//         conversion_map.insert(old_id, new_id);
-//     }
-//
-//     for (idx, chunk) in mca.iter().flatten().enumerate() {
-//         println!("Converted {} chunks", idx);
-//         let chunk: Result<chunk147::Chunk> = fastnbt::from_bytes(&chunk.data);
-//         let chunk = chunk.unwrap();
-//         let new_chunk = chunk1201::Chunk::convert_old(&chunk, &conversion_map);
-//         converted_mca
-//             .write_chunk(
-//                 chunk.level().x_pos() as usize,
-//                 chunk.level().z_pos() as usize,
-//                 fastnbt::to_bytes(&new_chunk)
-//                     .expect("can't convert chunk to bytes")
-//                     .as_slice(),
-//             )
-//             .expect("can't write chunk to file");
-//     }
-//     println!("Converted all chunks");
-// }
-
 enum ChunkMessage {
     JOIN,
     CHUNK(Vec<u8>),
@@ -74,7 +19,6 @@ fn replace_all_old_file(
     path: String,
     converted_path: String,
     conversion_map: Arc<RwLock<HashMap<chunk147::Block, chunk147::Block>>>,
-    data_map: Arc<RwLock<HashMap<chunk147::Block, i8>>>,
 ) {
     let file = File::open(path.clone()).unwrap();
     let mut open_options = OpenOptions::new();
@@ -85,22 +29,17 @@ fn replace_all_old_file(
         .read(true);
     let converted_file = open_options.open(converted_path.clone()).unwrap();
 
-    // println!("Reading input file {path}");
     let mut mca = Region::from_stream(file).unwrap();
-    // println!("Creating output file {converted_path}");
     let mut converted_mca = Region::new(converted_file).unwrap();
 
-    // println!("Initializing data channels");
     let (tx_c, rx_c) = flume::unbounded();
     let (tx_r, rx_r) = flume::unbounded();
     let mut threads = vec![];
 
-    // println!("Initializing chunk threads");
     for _ in 0..thread::available_parallelism().unwrap().get() {
         let rx_c = rx_c.clone();
         let tx_r = tx_r.clone();
         let conversion_map = conversion_map.clone();
-        let data_map = data_map.clone();
         threads.push(thread::spawn(move || loop {
             let message: ChunkMessage = rx_c.recv().unwrap();
             if let ChunkMessage::CHUNK(chunk_data) = message {
@@ -110,15 +49,10 @@ fn replace_all_old_file(
                         tile_entity.id() == "Chest"
                             || tile_entity.id() == "Sign"
                             || tile_entity.id() == "Skull"
+                            || tile_entity.id() == "MobSpawner"
                     });
                     for section in chunk.mut_level().mut_sections().iter_mut() {
-                        for (key, value) in conversion_map.read().unwrap().iter() {
-                            if let Some(data) = data_map.read().unwrap().get(key) {
-                                section.replace_all_data(*key, *value, *data);
-                            } else {
-                                section.replace_all(*key, *value);
-                            }
-                        }
+                        section.replace_all_blocks(conversion_map.clone());
                     }
                     tx_r.send(chunk).unwrap();
                 } else {
@@ -132,7 +66,6 @@ fn replace_all_old_file(
 
     let mut read = 0;
 
-    // println!("Reading chunks");
     for chunk in mca.iter().flatten() {
         read += 1;
         tx_c.send(ChunkMessage::CHUNK(chunk.data)).unwrap();
@@ -140,7 +73,6 @@ fn replace_all_old_file(
 
     let mut converted = 0;
 
-    // println!("Starting receiving converted chunks");
     if read > 0 {
         while let Ok(chunk) = rx_r.recv() {
             converted += 1;
@@ -159,7 +91,6 @@ fn replace_all_old_file(
         }
     }
 
-    // println!("Joining chunk threads");
     for _ in &threads {
         tx_c.send(ChunkMessage::JOIN).unwrap();
     }
@@ -167,49 +98,45 @@ fn replace_all_old_file(
     for thread in threads {
         thread.join().unwrap();
     }
-
-    // println!("Converted all chunks");
 }
 
 fn read_conversion_file(
     conversion_path: String,
-) -> (
-    Arc<RwLock<HashMap<chunk147::Block, chunk147::Block>>>,
-    Arc<RwLock<HashMap<chunk147::Block, i8>>>,
-) {
+) -> Arc<RwLock<HashMap<chunk147::Block, chunk147::Block>>> {
     let conversion_map = Arc::new(RwLock::new(HashMap::new()));
-    let data_map = Arc::new(RwLock::new(HashMap::new()));
-    // println!("Opening {}", conversion_path);
     let mut conversion_file = File::open(conversion_path).unwrap();
     let mut conversion_content = String::new();
-    // println!("Reading conversion file");
     conversion_file
         .read_to_string(&mut conversion_content)
         .unwrap();
 
     for line in conversion_content.lines() {
         let mut split = line.split("->");
-        let old_id = split
-            .nth(0)
-            .unwrap()
-            .trim()
-            .parse::<chunk147::Block>()
-            .unwrap();
+        let o = split.nth(0).unwrap().trim();
+        let old_id = if o.contains(":") {
+            let mut n_split = o.split(":");
+            let id = n_split.nth(0).unwrap().parse::<i32>().unwrap();
+            let data = n_split.nth(0).unwrap().parse::<i8>().unwrap();
+
+            chunk147::Block::new(id, Some(data))
+        } else {
+            chunk147::Block::new(o.parse().unwrap(), None)
+        };
         let n = split.nth(0).unwrap().trim();
         let new_id = if n.contains(":") {
             let mut n_split = n.split(":");
-            let id = n_split.nth(0).unwrap().parse::<chunk147::Block>().unwrap();
+            let id = n_split.nth(0).unwrap().parse::<i32>().unwrap();
             let data = n_split.nth(0).unwrap().parse::<i8>().unwrap();
 
-            data_map.write().unwrap().insert(old_id, data);
-            id
+            chunk147::Block::new(id, Some(data))
         } else {
-            n.parse::<chunk147::Block>().unwrap()
+            chunk147::Block::new(n.parse().unwrap(), None)
         };
+
         conversion_map.write().unwrap().insert(old_id, new_id);
     }
 
-    (conversion_map, data_map)
+    conversion_map
 }
 
 fn replace_all_old() {
@@ -219,14 +146,14 @@ fn replace_all_old() {
         let path = std::env::args().nth(3).unwrap();
         let converted_path = std::env::args().nth(4).unwrap();
 
-        let (conversion_map, data_map) = read_conversion_file(conversion_path);
+        let conversion_map = read_conversion_file(conversion_path);
 
-        replace_all_old_file(path, converted_path, conversion_map, data_map);
+        replace_all_old_file(path, converted_path, conversion_map);
     } else {
         let conversion_path = std::env::args().nth(2).unwrap();
         let dir = std::env::args().nth(3).unwrap();
         let out_dir = std::env::args().nth(4).unwrap();
-        let (conversion_map, data_map) = read_conversion_file(conversion_path);
+        let conversion_map = read_conversion_file(conversion_path);
         let mut files = vec![];
         for file in fs::read_dir(dir.clone()).unwrap() {
             let f = file.unwrap();
@@ -247,7 +174,6 @@ fn replace_all_old() {
             converted_path.push(file.clone());
 
             let conversion_map = conversion_map.clone();
-            let data_map = data_map.clone();
 
             let counter = counter.clone();
 
@@ -256,7 +182,6 @@ fn replace_all_old() {
                     path.to_str().unwrap().to_string(),
                     converted_path.to_str().unwrap().to_string(),
                     conversion_map,
-                    data_map,
                 );
                 counter.fetch_add(1, Ordering::SeqCst);
             });
