42b152619ed9bec4d15bb86d1d14f3a086cc5922
enn3 <arturpeshko39@gmail.com>
2024-02-02 14:58:44 +0100

    better conversion

--- src/chunk147.rs
+++ src/chunk147.rs
@@ -286,7 +286,7 @@ impl Section {
                     0
                 };
                 if old.data.is_none() || old.data.unwrap() == data {
-                    if old.id < 256 {
+                    if old.add_to_i8() == 0 {
                         conversion.push((idx, new));
                     } else if let Some(added) = &self.add {
                         let d = if idx % 2 == 0 {
