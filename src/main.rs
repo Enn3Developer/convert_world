89a6a53b3ddaf103aff39b976194502eb5885932
enn3 <arturpeshko39@gmail.com>
2024-03-27 14:11:23 +0100

    testing async

--- src/main.rs
+++ src/main.rs
@@ -219,14 +219,17 @@ async fn replace_all_old() {
             pauses += (Instant::now() - start_pause).as_secs_f32();
 
             println!("Signaling all workers");
+            let local_start = Instant::now();
             broadcast.send(true).unwrap();
             while let Some(_handle) = handles.join_next().await {
                 i += 1;
-                let elapsed = (Instant::now() - start).as_secs_f32() - pauses;
-                let mean_rps = if elapsed == 0.0 {
+                let now = Instant::now();
+                let elapsed = (now - start).as_secs_f32() - pauses;
+                let local_elapsed = (now - local_start).as_secs_f32();
+                let mean_rps = if local_elapsed == 0.0 {
                     0.0
                 } else {
-                    i as f32 / elapsed
+                    i as f32 / local_elapsed
                 };
                 let eta = if mean_rps == 0.0 {
                     0.0
