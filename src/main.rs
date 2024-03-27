16d17e288a4de6b91a2d892b402ea971e26b805c
enn3 <arturpeshko39@gmail.com>
2024-03-27 01:09:42 +0100

    testing async

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ use std::sync::Arc;
 use std::time::Instant;
 use tikv_jemallocator::Jemalloc;
 use tokio::sync::{Mutex, RwLock};
+use tokio::task::JoinSet;
 use tokio_stream::StreamExt;
-use tokio_tasker::Tasker;
 
 #[global_allocator]
 static GLOBAL: Jemalloc = Jemalloc;
@@ -37,54 +37,66 @@ async fn replace_all_old_file(
                 .unwrap(),
         ));
 
-        let conversion_map = conversion_map.read().await;
+        let mut handles = JoinSet::new();
         for chunk in mca.iter() {
             if let Ok(chunk) = chunk {
-                let chunk_data = chunk.data;
-                let mut chunk = tokio::task::spawn_blocking(move || {
-                    fastnbt::from_bytes::<chunk147::Chunk>(&chunk_data)
-                })
-                .await
-                .unwrap()
-                .unwrap();
-                let tile_entities =
-                    tokio_stream::iter(chunk.level().tile_entities().clone().into_iter())
-                        .filter(|tile_entity| {
-                            tile_entity.id() == "Chest"
-                                || tile_entity.id() == "Sign"
-                                || tile_entity.id() == "Skull"
-                                || tile_entity.id() == "MobSpawner"
-                        })
-                        .map(|tile_entity| tile_entity.clone())
-                        .collect::<Vec<_>>()
-                        .await;
-                chunk.mut_level().set_tile_entities(tile_entities);
+                let conversion_map = conversion_map.read().await.clone();
+                let region = region.clone();
+                handles.spawn(async move {
+                    let chunk_data = chunk.data;
+                    let mut chunk = tokio::task::spawn_blocking(move || {
+                        fastnbt::from_bytes::<chunk147::Chunk>(&chunk_data)
+                    })
+                    .await
+                    .unwrap()
+                    .unwrap();
+                    let tile_entities =
+                        tokio_stream::iter(chunk.level().tile_entities().clone().into_iter())
+                            .filter(|tile_entity| {
+                                tile_entity.id() == "Chest"
+                                    || tile_entity.id() == "Sign"
+                                    || tile_entity.id() == "Skull"
+                                    || tile_entity.id() == "MobSpawner"
+                            })
+                            .map(|tile_entity| tile_entity.clone())
+                            .collect::<Vec<_>>()
+                            .await;
+                    chunk.mut_level().set_tile_entities(tile_entities);
 
-                let mut stream =
-                    tokio_stream::iter(conversion_map.iter().zip(chunk.mut_level().mut_sections()));
+                    let mut stream = tokio_stream::iter(
+                        conversion_map.iter().zip(chunk.mut_level().mut_sections()),
+                    );
 
-                while let Some(((old_block, new_block), section)) = stream.next().await {
-                    section.replace_all(old_block, new_block).await;
-                }
+                    while let Some(((old_block, new_block), section)) = stream.next().await {
+                        section.replace_all(old_block, new_block).await;
+                    }
 
-                let mut buf = vec![];
-                let uncompressed_chunk =
-                    fastnbt::to_bytes(&chunk).expect("can't convert chunk to bytes");
-                let mut enc = ZlibEncoder::new(uncompressed_chunk.as_slice(), Compression::best());
-                enc.read_to_end(&mut buf).unwrap();
-                let write = region.lock().await.write_compressed_chunk(
-                    (chunk.level().x_pos() as usize) % 32,
-                    (chunk.level().z_pos() as usize) % 32,
-                    CompressionScheme::Zlib,
-                    &buf,
-                );
+                    let mut buf = vec![];
+                    let uncompressed_chunk =
+                        fastnbt::to_bytes(&chunk).expect("can't convert chunk to bytes");
+                    let mut enc =
+                        ZlibEncoder::new(Cursor::new(uncompressed_chunk), Compression::best());
+                    let buf = tokio::task::spawn_blocking(move || {
+                        enc.read_to_end(&mut buf).unwrap();
+                        buf
+                    })
+                    .await
+                    .unwrap();
+                    let write = region.lock().await.write_compressed_chunk(
+                        (chunk.level().x_pos() as usize) % 32,
+                        (chunk.level().z_pos() as usize) % 32,
+                        CompressionScheme::Zlib,
+                        &buf,
+                    );
 
-                if let Err(Error::InvalidOffset(x, z)) = write {
-                    println!("can't write chunk at x: {x}, z: {z}");
-                }
+                    if let Err(Error::InvalidOffset(x, z)) = write {
+                        println!("can't write chunk at x: {x}, z: {z}");
+                    }
+                });
             }
         }
-        drop(mca);
+
+        while let Some(_handle) = handles.join_next().await {}
 
         let region = Arc::into_inner(region).unwrap().into_inner();
         tokio::fs::write(converted_path, region.into_inner().unwrap().into_inner())
@@ -160,7 +172,7 @@ async fn replace_all_old() {
         let out_dir = std::env::args().nth(4).unwrap();
         let conversion_map = read_conversion_file(conversion_path).await;
         let mut read_dir = tokio::fs::read_dir(dir.clone()).await.unwrap();
-        let handles = Tasker::new();
+        let mut handles = JoinSet::new();
         println!("Starting all workers");
         let mut started = 0;
         let start = Instant::now();
@@ -187,9 +199,8 @@ async fn replace_all_old() {
 
         let mut i = 0;
         let len = started;
-        let mut stream = handles.join_stream();
 
-        while let Some(_handle) = stream.next().await {
+        while let Some(_handle) = handles.join_next().await {
             i += 1;
             let elapsed = (Instant::now() - start).as_secs_f32();
             let mean_rps = if elapsed == 0.0 {
